8bc792941e7ce76b7cc3aaa6250470305b0ba447
Artem Kulikov <chigar2010@yandex.ru>
2024-08-08 23:53:36 +0200

    upd

--- app/api/meeting_room.py
+++ app/api/meeting_room.py
@@ -1,22 +1,115 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.crud.meeting_room import create_meeting_room, get_room_id_by_name
-from app.schemas.meeting_room import MeetingRoomCreate
+from app.core.db import get_async_session
+from app.crud.meeting_room import create_meeting_room, get_room_id_by_name, \
+    read_all_rooms_from_db, update_meeting_room, get_meeting_room_by_id, \
+    delete_meeting_room
+from app.models.meeting_room import MeetingRoom
+from app.schemas.meeting_room import MeetingRoomCreate, MeetingRoomDB, \
+    MeetingRoomUpdate
 
-router = APIRouter()
+router = APIRouter(prefix='/meeting_rooms', tags=['Meeting Rooms'])
 
 
-@router.post('/meeting_rooms/')
+@router.post(
+    '/',
+    response_model=MeetingRoomDB,
+    response_model_exclude_none=True,
+)
 async def create_new_meeting_room(
-        meeting_room: MeetingRoomCreate,
+    meeting_room: MeetingRoomCreate,
+    session: AsyncSession = Depends(get_async_session),
 ):
-    # Вызываем функцию проверки уникальности поля name:
-    room_id = await get_room_id_by_name(meeting_room.name)
-    # Если такой объект уже есть в базе - вызываем ошибку:
+    await check_name_duplicate(meeting_room.name, session)
+    new_room = await create_meeting_room(meeting_room, session)
+    return new_room
+
+
+@router.get(
+    '/',
+    response_model=list[MeetingRoomDB],
+    response_model_exclude_none=True,
+)
+async def get_all_meeting_rooms(
+    session: AsyncSession = Depends(get_async_session),
+):
+    return await read_all_rooms_from_db(session)
+
+
+@router.patch(
+    # ID обновляемого объекта будет передаваться path-параметром.
+    '/{meeting_room_id}',
+    response_model=MeetingRoomDB,
+    response_model_exclude_none=True,
+)
+async def partially_update_meeting_room(
+        # ID обновляемого объекта.
+        meeting_room_id: int,
+        # JSON-данные, отправленные пользователем.
+        obj_in: MeetingRoomUpdate,
+        session: AsyncSession = Depends(get_async_session),
+):
+    # Получаем объект из БД по ID.
+    # В ответ ожидается либо None, либо объект класса MeetingRoom.
+    meeting_room = await check_meeting_room_exists(
+        meeting_room_id, session
+    )
+
+    if obj_in.name is not None:
+        # Если в запросе получено поле name — проверяем его на уникальность.
+        await check_name_duplicate(obj_in.name, session)
+
+    # Передаём в корутину все необходимые для обновления данные.
+    meeting_room = await update_meeting_room(
+        meeting_room, obj_in, session
+    )
+    return meeting_room
+
+
+@router.delete(
+    '/{meeting_room_id}',
+    response_model=MeetingRoomDB,
+    response_model_exclude_none=True,
+)
+async def remove_meeting_room(
+        meeting_room_id: int,
+        session: AsyncSession = Depends(get_async_session),
+):
+    # Выносим повторяющийся код в отдельную корутину.
+    meeting_room = await check_meeting_room_exists(
+        meeting_room_id, session
+    )
+    meeting_room = await delete_meeting_room(
+        meeting_room, session
+    )
+    return meeting_room
+
+
+# Корутина, проверяющая уникальность полученного имени переговорки.
+async def check_name_duplicate(
+        room_name: str,
+        session: AsyncSession,
+) -> None:
+    room_id = await get_room_id_by_name(room_name, session)
     if room_id is not None:
         raise HTTPException(
             status_code=422,
             detail='Переговорка с таким именем уже существует!',
         )
-    new_room = await create_meeting_room(meeting_room)
-    return new_room
+
+
+# Оформляем повторяющийся код в виде отдельной корутины.
+async def check_meeting_room_exists(
+        meeting_room_id: int,
+        session: AsyncSession,
+) -> MeetingRoom:
+    meeting_room = await get_meeting_room_by_id(
+        meeting_room_id, session
+    )
+    if meeting_room is None:
+        raise HTTPException(
+            status_code=404,
+            detail='Переговорка не найдена!'
+        )
+    return meeting_room
--- app/schemas/meeting_room.py
+++ app/schemas/meeting_room.py
@@ -1,8 +1,33 @@
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
-class MeetingRoomCreate(BaseModel):
-    name: str = Field(..., min_length=1, max_length=100)
+# Базовый класс схемы, от которого наследуем все остальные.
+class MeetingRoomBase(BaseModel):
+    name: Optional[str] = Field(None, min_length=1, max_length=100)
     description: Optional[str]
+
+
+# Теперь наследуем схему не от BaseModel, а от MeetingRoomBase.
+class MeetingRoomCreate(MeetingRoomBase):
+    # Переопределяем атрибут name, делаем его обязательным.
+    name: str = Field(..., min_length=1, max_length=100)
+    # Описывать поле description не нужно: оно уже есть в базовом классе.
+
+
+class MeetingRoomUpdate(MeetingRoomBase):
+    @validator('name')
+    def name_cant_be_null(cls, value: str):
+        if value is None:
+            raise ValueError('Имя переговорки не может быть пустым!')
+        return value
+
+
+# Возвращаемую схему унаследуем от MeetingRoomCreate,
+# чтобы снова не описывать обязательное поле name.
+class MeetingRoomDB(MeetingRoomCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
